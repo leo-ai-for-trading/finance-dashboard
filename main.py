ea612542b71385f30ff877e087c3966055cd274a
Attilio Pittelli <70145737+leo-ai-for-trading@users.noreply.github.com>
2022-03-08 18:38:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import sqlite3 
 import streamlit as st
 import pandas as pd
 import praw 
+import yfinance as yf
+import plotly.graph_objects as go
+import plotly.express as px
+import sqlite3
+
 
 #streamlit run main.py
 
@@ -11,7 +17,10 @@ reddit = praw.Reddit(
 )
 
 st.sidebar.title("Navigation Bar")
-options = st.sidebar.selectbox("",("wallstreetbets","twitter","3"))
+options = st.sidebar.selectbox("",("wallstreetbets","Chart","3"))
+
+connection = sqlite3.connect('table.db')
+cursor = connection.cursor()
 
 st.title(options)
 
@@ -23,7 +32,40 @@ if options == "wallstreetbets":
     #data = pd.DataFrame(subm, columns=['id','url','title','comments','score','author','time','content'])
     #json format works
     st.write(subm)
+    num_days = st.sidebar.slider('Number of days',1,30,3)
+
+    cursor.execute("""
+        SELECT COUNT(*) AS num_mentions, symbol
+        FROM mention JOIN stock ON stock.id = mention.stock_id
+        WHERE date(dt) > current_date - interval '%s day'
+        GROUP BY stock_id, symbol
+        HAVING COUNT (symbol) > 10
+        ORDER BY num_mentions DESC
+    """, (num_days,))
+
+    counts = cursor.fetchall()
+    for c in counts:
+        st.write(c)
     
+    cursor.execute(""""
+        SELECT symbol, message, url, dt, username
+        FROM mention JOIN stock ON stock.id = mention.stock_id
+        ORDER BY dt DESC
+        LIMIT 10
+    """)
+
+    mentions = cursor.fetchall()
+    for m in mentions:
+        st.text(m['dt'])
+        st.text(m['symbol'])
+        st.text(m['message'])
+        st.text(m['url'])
+        st.text(m['username'])
+    
+    rows = cursor.fetchall()
+    st.write(rows)
+
+
 if options == "Chart":
     name = st.text_input("Insert ticker")
     #x = name.upper()
@@ -35,5 +77,4 @@ if options == "Chart":
         fig.update_xaxes(rangeslider_visible=True)
 
         st.write(fig)
-
 
